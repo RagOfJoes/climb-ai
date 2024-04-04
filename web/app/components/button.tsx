3b573657deaa2b48c0bfed7047cbf1554cd5b2de
RagOfJoes <apps@ragofjoes.dev>
2024-04-03 20:39:53 -0700

    feat: add button component

---
+++ web/app/components/button.tsx
@@ -0,0 +1,73 @@
+import type { ButtonHTMLAttributes } from "react";
+import { forwardRef } from "react";
+
+import { Slot } from "@radix-ui/react-slot";
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/cn";
+
+const variants = cva(
+	[
+		"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors",
+
+		"disabled:pointer-events-none disabled:opacity-50",
+		"focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+	],
+	{
+		variants: {
+			variant: {
+				default: cn(
+					"bg-primary text-primary-foreground",
+
+					"hover:bg-primary/90",
+				),
+				destructive: cn(
+					"bg-destructive text-destructive-foreground",
+
+					"hover:bg-destructive/90",
+				),
+				ghost: "hover:bg-accent hover:text-accent-foreground",
+				link: cn(
+					"text-primary underline-offset-4",
+
+					"hover:underline",
+				),
+				outline: cn(
+					"border bg-background",
+
+					"border hover:bg-accent hover:text-accent-foreground",
+				),
+				secondary: cn(
+					"bg-secondary text-secondary-foreground",
+
+					"hover:bg-secondary/80",
+				),
+			},
+			size: {
+				default: "h-10 px-4 py-2",
+				icon: "h-10 w-10",
+				lg: "h-11 rounded-md px-8",
+				sm: "h-9 rounded-md px-3",
+			},
+		},
+		defaultVariants: {
+			size: "default",
+			variant: "default",
+		},
+	},
+);
+
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+	VariantProps<typeof variants> & {
+		asChild?: boolean;
+	};
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+	({ className, variant, size, asChild = false, ...props }, ref) => {
+		const C = asChild ? Slot : "button";
+
+		return <C className={cn(variants({ variant, size, className }))} ref={ref} {...props} />;
+	},
+);
+Button.displayName = "Button";
